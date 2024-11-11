dfea15b57caf62830906a139cafa2681394962ee
yunuo46 <yunuo468@gmail.com>
2024-11-11 22:19:01 +0900

    fix: set allow origin

--- src/main/java/com/ttn/demo/global/config/WebMvcConfig.java
+++ src/main/java/com/ttn/demo/global/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
