8c88eaabefff9a0e8252dbabc28f4c98ab454316
yunuo46 <yunuo468@gmail.com>
2024-11-13 16:42:26 +0900

    fix: allow origins

--- src/main/java/com/ttn/demo/global/config/WebMvcConfig.java
+++ src/main/java/com/ttn/demo/global/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
